967aca7b41848dc55ede82478277c1c4cdf285ef
Shanthosh <shanthubolt@gmail.com>
2025-08-09 20:18:57 +0530

    added submit

--- src/components/formPreviewModal.tsx
+++ src/components/formPreviewModal.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Dialog,
   DialogTitle,
@@ -14,6 +15,7 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
 import type { FormTemplate } from "../types/form";
 
 interface FormPreviewModalProps {
@@ -23,89 +25,167 @@ interface FormPreviewModalProps {
 }
 
 const FormPreviewModal = ({ open, onClose, form }: FormPreviewModalProps) => {
+  // Setup react-hook-form
+  const { handleSubmit, control } = useForm({
+    defaultValues:
+      form?.fields?.reduce((acc: Record<string, unknown>, field) => {
+        acc[field.name] = field.type === "checkbox" ? [] : "";
+        return acc;
+      }, {} as Record<string, unknown>) ?? {},
+  });
+
+  // Handle submit
+  const onSubmit = (data: unknown) => {
+    console.log("Form Data:", data);
+
+    alert("Submitted! Check console for data.");
+
+    onClose();
+  };
+
   if (!form) return null;
 
   return (
     <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
       <DialogTitle>{form.title} - Preview</DialogTitle>
-      <DialogContent dividers>
-        {form.fields.map((field) => {
-          switch (field.type) {
-            case "text":
-            case "email":
-            case "password":
-            case "number":
-              return (
-                <TextField
-                  key={field.id}
-                  label={field.label}
-                  type={field.type}
-                  fullWidth
-                  margin="normal"
-                  placeholder={field.placeholder}
-                  required={field.required}
-                />
-              );
-            case "dropdown":
-              return (
-                <FormControl key={field.id} fullWidth margin="normal">
-                  <FormLabel>{field.label}</FormLabel>
-                  <Select>
-                    {field.options?.map((opt) => (
-                      <MenuItem key={opt} value={opt}>
-                        {opt}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              );
-            case "radio":
-              return (
-                <FormControl
-                  key={field.id}
-                  component="fieldset"
-                  margin="normal"
-                >
-                  <FormLabel>{field.label}</FormLabel>
-                  <RadioGroup>
-                    {field.options?.map((opt) => (
-                      <FormControlLabel
-                        key={opt}
-                        value={opt}
-                        control={<Radio />}
-                        label={opt}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DialogContent dividers>
+          {form.fields.map((field) => {
+            switch (field.type) {
+              case "text":
+              case "email":
+              case "password":
+              case "number":
+                return (
+                  <Controller
+                    key={field.id}
+                    name={field.name}
+                    control={control}
+                    rules={{ required: field.required }}
+                    render={({ field: controllerField }) => (
+                      <TextField
+                        {...controllerField}
+                        label={field.label}
+                        type={field.type}
+                        fullWidth
+                        margin="normal"
+                        placeholder={field.placeholder}
+                        required={field.required}
                       />
-                    ))}
-                  </RadioGroup>
-                </FormControl>
-              );
-            case "checkbox":
-              return (
-                <FormControl
-                  key={field.id}
-                  component="fieldset"
-                  margin="normal"
-                >
-                  <FormLabel>{field.label}</FormLabel>
-                  {field.options?.map((opt) => (
-                    <FormControlLabel
-                      key={opt}
-                      control={<Checkbox />}
-                      label={opt}
+                    )}
+                  />
+                );
+              case "dropdown":
+                return (
+                  <FormControl key={field.id} fullWidth margin="normal">
+                    <FormLabel>{field.label}</FormLabel>
+                    <Controller
+                      name={field.name}
+                      control={control}
+                      rules={{ required: field.required }}
+                      render={({ field: controllerField }) => (
+                        <Select {...controllerField}>
+                          {field.options?.map((opt) => (
+                            <MenuItem key={opt} value={opt}>
+                              {opt}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      )}
                     />
-                  ))}
-                </FormControl>
-              );
-            default:
-              return null;
-          }
-        })}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} variant="outlined">
-          Close
-        </Button>
-      </DialogActions>
+                  </FormControl>
+                );
+              case "radio":
+                return (
+                  <FormControl
+                    key={field.id}
+                    component="fieldset"
+                    margin="normal"
+                  >
+                    <FormLabel>{field.label}</FormLabel>
+                    <Controller
+                      name={field.name}
+                      control={control}
+                      rules={{ required: field.required }}
+                      render={({ field: controllerField }) => (
+                        <RadioGroup {...controllerField}>
+                          {field.options?.map((opt) => (
+                            <FormControlLabel
+                              key={opt}
+                              value={opt}
+                              control={<Radio />}
+                              label={opt}
+                            />
+                          ))}
+                        </RadioGroup>
+                      )}
+                    />
+                  </FormControl>
+                );
+              case "checkbox":
+                return (
+                  <FormControl
+                    key={field.id}
+                    component="fieldset"
+                    margin="normal"
+                  >
+                    <FormLabel>{field.label}</FormLabel>
+                    <Controller
+                      name={field.name}
+                      control={control}
+                      render={({ field: controllerField }) => (
+                        <>
+                          {field.options?.map((opt) => (
+                            <FormControlLabel
+                              key={opt}
+                              control={
+                                <Checkbox
+                                  checked={
+                                    (
+                                      (controllerField.value as string[]) ?? []
+                                    )?.includes(opt) ?? false
+                                  }
+                                  onChange={(e) => {
+                                    if (e.target.checked) {
+                                      controllerField.onChange([
+                                        ...(Array.isArray(controllerField.value)
+                                          ? controllerField.value
+                                          : []),
+                                        opt,
+                                      ]);
+                                    } else {
+                                      controllerField.onChange(
+                                        (Array.isArray(controllerField.value)
+                                          ? controllerField.value
+                                          : []
+                                        ).filter((v: string) => v !== opt)
+                                      );
+                                    }
+                                  }}
+                                />
+                              }
+                              label={opt}
+                            />
+                          ))}
+                        </>
+                      )}
+                    />
+                  </FormControl>
+                );
+              default:
+                return null;
+            }
+          })}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} variant="outlined">
+            Close
+          </Button>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
